ea9ca629c019a16466a0070677bbcdb2aaa6be2f
NallabellyVinay <52525335+NallabellyVinay@users.noreply.github.com>
2024-08-28 00:47:26 +0530

    Update Soundex.cs

--- Soundex.cs
+++ Soundex.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 public class Soundex
 {
+    // Mapping of consonants to Soundex codes
     private static readonly Dictionary<char, char> soundexMap = new Dictionary<char, char>
     {
         { 'B', '1' }, { 'F', '1' }, { 'P', '1' }, { 'V', '1' },
@@ -15,15 +16,16 @@ public class Soundex
         { 'R', '6' }
     };
 
+    // Generates the Soundex code for a given name
     public static string GenerateSoundex(string name)
     {
         if (string.IsNullOrEmpty(name))
         {
-            return string.Empty;
+            return string.Empty; // Handle empty or null string
         }
 
         StringBuilder soundex = new StringBuilder();
-        soundex.Append(char.ToUpper(name[0]));
+        soundex.Append(char.ToUpper(name[0])); // First letter of the name
         char prevCode = GetSoundexCode(name[0]);
 
         ProcessCharacters(name, soundex, ref prevCode);
@@ -31,6 +33,7 @@ public class Soundex
         return PadSoundex(soundex).ToString();
     }
 
+    // Processes the characters of the name to generate the Soundex code
     private static void ProcessCharacters(string name, StringBuilder soundex, ref char prevCode)
     {
         for (int i = 1; i < name.Length && soundex.Length < 4; i++)
@@ -39,6 +42,7 @@ public class Soundex
         }
     }
 
+    // Appends the Soundex code for a character if applicable
     private static void AppendSoundexCode(char c, StringBuilder soundex, ref char prevCode)
     {
         char code = GetSoundexCode(c);
@@ -49,11 +53,13 @@ public class Soundex
         }
     }
 
+    // Determines if the Soundex code should be appended
     private static bool IsAppendableCode(char code, char prevCode)
     {
         return code != '0' && code != prevCode;
     }
 
+    // Pads the Soundex code with zeros to ensure it is four characters long
     private static StringBuilder PadSoundex(StringBuilder soundex)
     {
         while (soundex.Length < 4)
@@ -64,6 +70,7 @@ public class Soundex
         return soundex;
     }
 
+    // Retrieves the Soundex code for a given character
     private static char GetSoundexCode(char c)
     {
         c = char.ToUpper(c);
